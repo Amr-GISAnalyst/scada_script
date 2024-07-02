287c29fd6ea1192f0e2b06049e40a8ba49137c1a
AmrAElhameed <amr_abdelhameed@outlook.com>
2024-07-02 09:59:27 +0300

    OOP edits

--- Scada.py
+++ Scada.py
@@ -1,39 +1,25 @@
-import arcpy
 import requests
+import arcpy
 import os
 from dotenv import load_dotenv, dotenv_values
+load_dotenv()
 
+class Scada:
+    def __init__(self):
+        self.DATABASE = os.getenv("GDB")
+        self.URL = os.getenv("URI")
+        self.arc_env()
 
-DATABASE = os.getenv("GDB")
-arcpy.env.overwriteOutput = True
-arcpy.env.workspace = DATABASE
+    def arc_env(self):
+        arcpy.env.overwriteOutput = True
+        arcpy.env.workspace = self.DATABASE
 
+    def pr(self):
+        print(self.DATABASE)
+        print(self.URL)
 
-class Deploy:
-    def __init__(self,featureclass):
-        self.URI = os.getenv("URL")
-        
-        load_dotenv()
-        self.end_wtp = {
-        "roundpoint": "RondPoint",
-        "manshia": "Manshia",
-        "manshia_2": "Manshia2",
-        "siouf": "Siouf",
-        "maamoura": "Maamoura",
-        "nozha": "Nozha",
-        "k40": "BA"}
-
-        self.fields = []
-
-        list = arcpy.ListFields(featureclass)
-        for field in list:
-            if field.name == "Shape" or field.name == "OBJECTID" or field.name == "sensor_id" or field.name == "wtp_name" or field.name == "description":
-                pass
-            else:
-                self.fields.append(field.name)
-
-    def request_data(self,dict_code):
-        response = requests.get(self.URI + (dict_code))
+    def request(self):
+        response = requests.get(self.URL + "roundpoint")
         response.raise_for_status()
         data = response.json()
         print(data)
--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-import requests
-import os
-import arcpy
-from Scada import Deploy
-#----------------------------------------------
-from dotenv import load_dotenv, dotenv_values
-#----------------------------------------------
+from scada import Scada
 
-#----------------------------------------------
-# setting env variables for AUTH to use it safely.
-print("setting env variables for AUTH to use it safely.\n ---------------------------------------")
-#---------------------------------------------------------------
-#listing fields in 3 featureclasses.
-print("listing fields in both featureclasses.\n ---------------------------------------")
-#------------------------------------------
-
-raw = Deploy("rawRecords")
-raw.request_data("manshia")
+edit = Scada()
+edit.pr()
+edit.request()
