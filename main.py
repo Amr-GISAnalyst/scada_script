4c365b64fd49c74346afc26be4f9986b8e2bb474
AmrAElhameed <amr_abdelhameed@outlook.com>
2024-07-04 13:23:11 +0300

    Edit session and looping through the .da cursor of update values

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ import os
 import arcpy
 import requests
 from dotenv import load_dotenv, dotenv_values
+
 load_dotenv()
 
 #env variable
 GDB = os.getenv("DATABASE")
 URL = os.getenv("LINK")
 
-arcpy.env.workspace = GDB
 arcpy.env.overwriteOutput = True
+arcpy.env.workspace = GDB
+
+sensor_fields = []
+
+fields = arcpy.ListFields("rawRecords")
+for field in fields:
+    if field.name == "Shape" or field.name == "OBJECTID" or field.name == "sensorid" or field.name == "description" or field.name == "x" or field.name == "y":
+        pass
+    else:
+        sensor_fields.append(field.name)       
 
 #Data
 treated = []
@@ -21,22 +31,17 @@ response = requests.get(URL + "roundpoint")
 response.raise_for_status()
 data = response.json()
 sensors = (data["data"])
-
-fields = []
-fields = arcpy.ListFields(GDB + "\\" +"pressureRecords")
-for field in fields:
-    if field.name == "Shape" or field.name == "OBJECTID" or field.name == "sensorid" or field.name == "description" or field.name == "GlobalID" or field.name == "x" or field.name == "y":
-        pass
-    else:
-        fields.append(field.name)
-
+print(sensors)
 #editting
+edit = arcpy.da.Editor(GDB)
+edit.startEditing(with_undo=False, multiuser_mode=False)
+edit.startOperation()
 for item in sensors:
     if item == "rawRecords":
         raw.extend(sensors["rawRecords"])
         # print(raw)
-        for i in range(len(raw)):
-             with arcpy.da.UpdateCursor(item,fields,f"raw[i]['TagName'] = {'sensorid'}") as input_rows:
+        for sensor in range(len(raw)):
+             with arcpy.da.UpdateCursor(item,fields,f"raw[sensor]['TagName'] == {'sensorid'}") as input_rows:
                 for row in input_rows:
                         for i in range(len(fields)):
                             if raw[i]['last_value'] == "0.00":
@@ -45,12 +50,5 @@ for item in sensors:
                             else:
                                 row[i] = float(raw[i]['last_value'])
                         input_rows.updateRow(row)
-
-#     elif item == "treatedRecords":
-#         treated.extend(sensors["treatedRecords"])
-#         print(treated)
-#     else:
-#         pressure.extend(sensors["pressureRecords"])
-#         print(pressure)
-
-# print(sensors)
+edit.stopOperation()
+edit.stopEditing(save_changes=True)
