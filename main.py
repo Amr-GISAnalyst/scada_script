bc20dddc56e4142b29cbc79e163bc25bb8aeaf8b
AmrAElhameed <amr_abdelhameed@outlook.com>
2024-07-09 09:56:06 +0300

    relocate project.

--- main.py
+++ main.py
@@ -1,60 +1,62 @@
 import arcpy
 import os
 import requests
-from dotenv import load_dotenv
-
-load_dotenv()
-
 #env variable
-GDB = os.getenv("DATABASE")
-URL = os.getenv("LINK")
-print(URL)
-# arcpy.env.overwriteOutput = True
-# arcpy.env.workspace = GDB
+print("setting env variables----------------------------")
+GDB = os.environ.get('DATABASE')
+URL = os.environ.get('LINK')
 
-# sensor_fields = []
+arcpy.env.overwriteOutput = True
+arcpy.env.workspace = GDB
 
-# fields = arcpy.ListFields("GIS.APP_Features\\GIS.rawRecords")
-# for field in fields:
-#     if field.name == "Shape" or field.name == "OBJECTID" or field.name == "sensorid" or field.name == "description" or field.name == "X" or field.name == "Y":
-#         pass
-#     else:
-#         sensor_fields.append(field.name)       
-# print(sensor_fields)
-#Data
-# treated = []
-# raw = []
-# pressure = []
+sensor_fields = []
 
-# #request
-# response = requests.get(URL + "roundpoint")
-# response.raise_for_status()
-# data = response.json()
-# sensors = (data["data"])
-# # print(sensors)
+#listing fields
+print("Listing fields------------------------------------")
+fields = arcpy.ListFields("rawRecords")
+for field in fields:
+    if field.name == "Shape" or field.name == "OBJECTID" or field.name == "sensorid" or field.name == "description" or field.name == "X" or field.name == "Y":
+        pass
+    else:
+        sensor_fields.append(field.name)       
+print(sensor_fields)
 
-# for item in sensors:
-#     if item == "rawRecords":
-#         raw.extend(sensors["rawRecords"])
-#     elif item == "treatedRecords":
-#         treated.extend(sensors["treatedRecords"])
-#     else:
-#         pressure.extend(sensors["pressureRecords"])
+treated = []
+raw = []
+pressure = []
 
-# #editting
-# edit = arcpy.da.Editor(GDB)
-# edit.startEditing(with_undo=False, multiuser_mode=True)
-# edit.startOperation()
-# for sensor in range(len(raw)):
-#     for tag in raw[sensor]["TagName"]:
-#         with arcpy.da.UpdateCursor(in_table= "GIS.APP_Features\\GIS.rawRecords", field_names= sensor_fields, where_clause= f"sensorid = {tag}") as data_edit:
-#             for row in data_edit:
-#                 for i in range(len(sensor_fields)):
-#                     if raw[sensor]["last_value"] == "0.00":
-#                         row[i] = None #continue
-#                         data_edit.updateRow(row) 
-#                     else:
-#                         row[i] = float(raw[sensor]["last_value"])
-#                 data_edit.updateRow(row)
+#request
+print("Requesting API-------------------------------------")
+response = requests.get(URL + "roundpoint")
+response.raise_for_status()
+data = response.json()
+sensors = (data["data"])
+# print(sensors)
+
+for item in sensors:
+    if item == "rawRecords":
+        raw.extend(sensors["rawRecords"])
+    elif item == "treatedRecords":
+        treated.extend(sensors["treatedRecords"])
+    else:
+        pressure.extend(sensors["pressureRecords"])
+
+#editting
+print("Starting Editting----------------------------------")
+edit = arcpy.da.Editor(GDB)
+edit.startEditing(with_undo=False, multiuser_mode=True)
+edit.startOperation()
+for sensor in range(len(raw)):
+    tg = raw[sensor]["TagName"]
+print(tg)    
+#     with arcpy.da.UpdateCursor("sensors\\rawRecords", sensor_fields, f"sensorid = {tg}") as data_edit:
+#         for row in data_edit:
+#             for i in range(len(sensor_fields)):
+#                 if raw[sensor]["last_value"] == "0.00":
+#                     row[i] = None #continue
+#                     data_edit.updateRow(row) 
+#                 else:
+#                     row[i] = float(raw[sensor]["last_value"])
+#             data_edit.updateRow(row)
 # edit.stopOperation()
 # edit.stopEditing(save_changes=True)
